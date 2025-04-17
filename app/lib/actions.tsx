a7914e92f4a591e4a594fdc50e1796b9cd38cff0
yanqzsu <yanqzsu@hotmail.com>
2025-04-17 17:04:37 +0800

    revalidate home

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -101,6 +101,7 @@ export async function updateInvoice(
   }
  
   revalidatePath('/dashboard/invoices');
+  revalidatePath('/dashboard');
   redirect('/dashboard/invoices');
 }
 
@@ -109,6 +110,7 @@ export async function deleteInvoice(id: string) {
   // throw new Error('Failed to Delete Invoice');
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
+  revalidatePath('/dashboard');
 }
 
 export async function authenticate(
