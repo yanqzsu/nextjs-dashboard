6c5c0e54e8de02fb5bfb82db7109d7f87c6fc761
yanqzsu <yanqzsu@hotmail.com>
2025-04-17 16:12:01 +0800

    lint

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -57,6 +57,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
     } catch (error) {
+      console.error('Database Error:', error);
       // If a database error occurs, return a more specific error.
       return {
         message: 'Database Error: Failed to Create Invoice.',
@@ -94,6 +95,7 @@ export async function updateInvoice(
       WHERE id = ${id}
     `;
   } catch (error) {
+    console.error('Database Error:', error);
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
  
